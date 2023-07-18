6d2e59f520a34b9c89474bd6ff001d214368bb4b
RohitNathie <103172111+RohitNathie@users.noreply.github.com>
2023-07-18 13:23:24 +0200

    tool-instroom gemaakt

---
+++ tool-instroom.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--- Head -->
+<?php include "./templates/head.php" ?>
+
+<body>
+
+    <!--- Header -->
+    <?php include "./templates/header.php" ?>
+
+    <!-- Parent container -->
+    <div class="parent-container">
+
+        <!--- Particle.js -->
+        <div id="particles-js"> </div>
+
+        <!--- Content -->
+        <div class="child-container">
+
+            <head>
+                
+            </head>
+
+            <body class="#tool-categories">
+
+                <div class="row text-center justify-content-center" style="margin: -75px 0px 0px 0px;">
+                    <a href="#" style="text-decoration: none;" class="tool-link text-dark">
+                        <div class="display-4">
+                            <i class="bi bi-arrow-right-short icon"></i>
+                        </div>
+                        <h4 class="p-2">Instroom</h4>
+                    </a>
+                </div>
+
+                <div class="fluid-container row tool-selection-container" style="margin: 100px 0px 0px 0px;">
+                    <a href="">
+                        <div class="col-lg-4 categorie-link">
+
+                            <h3>Employer Branding</h3>
+
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="col-lg-4 categorie-link">
+                            <h3>Onboarding</h3>
+
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="col-lg-4 categorie-link">
+                            <h3>Vacature</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="fluid-container row tool-selection-container" style="margin: 40px 0px 0px 0px;">
+
+
+                    <a href="">
+                        <div class="col-lg-4 categorie-link">
+                            <h3> Werven met een podcast</h3>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="col-lg-4 categorie-link">
+                            <h3>Inclusief</h3>
+                        </div>
+                    
+                    <a href="">
+                        <div class="col-lg-4"> </div>
+                </div></a>
+
+
+                <script src="app.js"></script>
+                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                <script src="offcanvas.js"></script>
+            </body>
+
+            </html>
+
+
+
+        </div>
+        <!--- Content ends here -->
+
+        <!--- Footer -->
+        <?php include "./templates/footer.php" ?>
+    </div>
+    </div>
+</body>
+
+</html>
