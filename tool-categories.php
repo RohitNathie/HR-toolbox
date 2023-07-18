9068fbfc9817c4d49ceae98d7d9f492258dd6ec2
MarioPfaff <mariojuliaanpfaff@gmail.com>
2023-07-18 14:10:01 +0200

    Tool categories edit

--- tool-categories.php
+++ tool-categories.php
@@ -17,48 +17,48 @@
 
         <!--- Content -->
         <div class="child-container">
-                <div class="row text-center justify-content-center" style="margin: -75px 0px 0px 0px;">
-                    <a href="#" style="text-decoration: none;" class="tool-link text-dark">
-                        <div class="display-4">
-                            <i class="bi bi-arrow-right-short icon"></i>
-                        </div>
-                        <h4 class="p-2">Doorstroom</h4>
-                    </a>
-                </div>
+            <div class="row text-center justify-content-center" style="margin: -75px 0px 0px 0px;">
+                <a href="#" style="text-decoration: none;" class="tool-link text-dark">
+                    <div class="display-4">
+                        <i class="bi bi-arrow-right-short icon"></i>
+                    </div>
+                    <h4 class="p-2">Instroom</h4>
+                </a>
+                <a href="tool-categories.php" style="text-decoration: none;" class="tool-link text-dark">
+                    <div class="display-4">
+                        <i class="bi bi-arrow-repeat icon"></i>
+                    </div>
+                    <h4 class="p-2">Doorstroom</h4>
+                </a>
+                <a href="#" style="text-decoration: none;" class="tool-link text-dark">
+                    <div class="display-4">
+                        <i class="bi bi-arrows-move icon"></i>
+                    </div>
+                    <h4 class="p-2">Uitstroom</h4>
+                </a>
+            </div>
 
-                <div class="fluid-container row tool-selection-container" style="margin: 100px 0px 0px 0px;">
-                    <a href="">
-                        <div class="col-lg-4 categorie-link">
-                            <h3>Behoud Top 10</h3>
-                        </div>
-                    </a>
-                    <a href="">
-                        <div class="col-lg-4 categorie-link">
-                            <h3>Gedragscode</h3>
-                        </div>
-                    </a>
-                    <a href="#" style="text-decoration: none !important;">
-                        <div class="col-lg-4 categorie-link">
-                            <h3>Vacature</h3>
-                        </div>
-                    </a>
-                </div>
-                <div class="fluid-container row tool-selection-container">
-                    <a href="">
-                        <div class="col-lg-4 categorie-link">
-                            <h3> Werven met een podcast</h3>
-                        </div>
-                    </a>
-                    <a href="">
-                        <div class="col-lg-4 categorie-link">
-                            <h3>Inclusief</h3>
-                        </div>
-                    </a>
-                </div>
-                <!--- Content ends here -->
+            <div class="fluid-container row tool-selection-container" style="margin: 50px 0px 0px 0px;">
+                <a href="" class="col-lg-4 col-12 categorie-link">
+                    <h3>Behoud Top 10</h3>
+                </a>
+                <a href="" class="col-lg-4 col-12 categorie-link">
+                    <h3>Gedragscode</h3>
+                </a>
+                <a href="#" class="col-lg-4 col-12 categorie-link">
+                    <h3>Gesprekken met medewerkers</h3>
+                </a>
+                <a href="" class="col-lg-4 col-4 col-12 categorie-link">
+                    <h3>Intervisie</h3>
+                </a>
+                <a href="" class="col-lg-4 col-4 col-12 categorie-link">
+                    <h3>Leven Lang Ontwikkelen</h3>
+                </a>
+            </div>
+            <!--- Content ends here -->
 
-                <!--- Footer -->
-                <?php include "./templates/footer.php" ?>
+            <!--- Footer -->
+            <?php include "./templates/footer.php" ?>
         </div>
 </body>
 
