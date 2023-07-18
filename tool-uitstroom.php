2d6ee8c89d288d34324fc5dce3adcb9addc1d5d8
RohitNathie <103172111+RohitNathie@users.noreply.github.com>
2023-07-18 13:46:43 +0200

    created tool-uitstroom

---
+++ tool-uitstroom.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!--- Head -->
+<?php include "./templates/head.php" ?>
+
+<body>
+
+    <!--- Header -->
+    <?php include "./templates/header.php" ?>
+
+    <!-- Parent container -->
+    <div class="parent-container">
+
+        <!--- Particle.js -->
+        <div id="particles-js"> </div>
+
+        <!--- Content -->
+        <div class="child-container">
+    
+            
+
+        </div>
+        <!--- Content ends here -->
+
+        <!--- Footer -->
+        <?php include "./templates/footer.php" ?>
+    </div>
+    </div>
+</body>
+
+</html>
