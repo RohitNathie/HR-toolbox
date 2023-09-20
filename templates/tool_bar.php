62ec377e4623da4304ac6327e01ffa4be3f56ac6
MarioPfaff <mariojuliaanpfaff@gmail.com>
2023-09-20 13:58:56 +0200

    Added animation, changed vh

--- templates/tool_bar.php
+++ templates/tool_bar.php
@@ -1,47 +1,47 @@
         <!-- Let op! Er moet nog een fallback gemaakt worden voor history.back, voor het geval als er geen history is. -->
-       
-  
-<div class="tool-bar mt-3 col-12 flex-grow-1 d-flex text-center" id="navbar-toolbar">
-<div style="position: absolute; left:20px; top:10px;"
-      <button onclick="history.back()" style="text-decoration: none; border: none; background-color: #fff0;" class="text-dark">
-            <div class="display-4">
-                <i class="bi bi-arrow-left-short icon"></i> <h6 class="p-2 text-center">Terug</h4>
+
+
+        <div class="tool-bar mt-3 col-12 flex-grow-1 d-flex text-center back-button" id="navbar-toolbar">
+            <div style="position: absolute; left:20px; top:10px;" <button onclick="history.back()" style="text-decoration: none; border: none; background-color: #fff0;" class="text-dark">
+                <div class="display-4 back-button">
+                    <i class="bi bi-arrow-left-short icon"></i>
+                    <h6 class="p-2 text-center">Terug</h4>
+                </div>
+                </button>
             </div>
-        </button>
-    </div>
 
 
 
-    <div class="col-4 flex-grow-1 tool-link">
-        <a href="/instroom.php" style="text-decoration: none;" class="text-dark">
-            <div class="display-4">
-                <i class="bi bi-arrow-right-short icon"></i>
+            <div class="col-4 flex-grow-1 tool-link">
+                <a href="/instroom.php" style="text-decoration: none;" class="text-dark">
+                    <div class="display-4">
+                        <i class="bi bi-arrow-right-short icon"></i>
+                    </div>
+                    <div class="d-none d-xl-block">
+                        <h6 class="p-2">Instroom</h4>
+                    </div>
+                </a>
             </div>
-            <div class="d-none d-xl-block">
-                <h6 class="p-2">Instroom</h4>
-            </div>
-        </a>
-    </div>
 
-    <div class="col-4 tool-link">
-        <a href="/doorstroom.php" style="text-decoration: none;" class="text-dark">
-            <div class="display-4">
-                <i class="bi bi-arrow-repeat icon"></i>
+            <div class="col-4 tool-link">
+                <a href="/doorstroom.php" style="text-decoration: none;" class="text-dark">
+                    <div class="display-4">
+                        <i class="bi bi-arrow-repeat icon"></i>
+                    </div>
+                    <div class="d-none d-xl-block">
+                        <h6 class="p-2">Doorstroom</h4>
+                    </div>
+                </a>
             </div>
-            <div class="d-none d-xl-block">
-                <h6 class="p-2">Doorstroom</h4>
-            </div>
-        </a>
-    </div>
 
-    <div class="col-4 tool-link">
-        <a href="/uitstroom.php" style="text-decoration: none;" class="text-dark">
-            <div class="display-4">
-                <i class="bi bi-arrows-move icon"></i>
+            <div class="col-4 tool-link">
+                <a href="/uitstroom.php" style="text-decoration: none;" class="text-dark">
+                    <div class="display-4">
+                        <i class="bi bi-arrows-move icon"></i>
+                    </div>
+                    <div class="d-none d-xl-block">
+                        <h6 class="p-2">Uitstroom</h4>
+                    </div>
+                </a>
             </div>
-            <div class="d-none d-xl-block">
-                <h6 class="p-2">Uitstroom</h4>
-            </div>
-        </a>
-    </div>
-</div>
+        </div>
