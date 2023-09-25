06fdd2bf626b0dbfa58764010ded083055615505
MarioPfaff <mariojuliaanpfaff@gmail.com>
2023-09-25 17:12:57 +0200

    Tekst veranderingen info.php en tool-selectie

--- info.php
+++ info.php
@@ -25,18 +25,14 @@
 
                     <p>Zij zijn je grootste kapitaal. In deze toolbox vind je instrumenten, stappenplannen en voorbeelden van andere werkgevers. Het bevat alles wat wel nodig is, maar niet wettelijk verplicht. Vooral bedoeld om jou als werkgever te helpen om je HR-zaken zo goed mogelijk vorm te geven. Blader door de pagina’s en download wat jij nodig hebt. Alles is zo gemaakt dat je er direct mee aan de slag kan. </p>
 
-                    <p>De HR toolbox is een initiatief van VNO-NCW Midden </p>
-
-                    <p>September 2023 is versie 1.0 opgeleverd. De wens is om de toolbox verder door te ontwikkelen. </p>
-
-                    <p>De HR-toolbox is ondersteund door RW-POA en de website is gemaakt door studenten van Roc Rivor. </p>
+                    <p>De HR toolbox is een initiatief van VNO-NCW Midden en wordt ondersteund door RW-POA. De website is gemaakt door studenten van ROC Rivor. </p>
 
                     <img src="/resources/images/roc.png" alt="ROC Rivor Logo" class="small_logo">
                     <img src="/resources/images/rw-poa.png" alt="RW-POA Logo" class="small_logo">
                     <img src="/resources/images/logo_v2.png" alt="VNO NCW Midden Logo" class="small_logo">
                     <br><br>
                     <h2>Achtergrond Info</h2>
-                    <p> De HR toolbox is in 2023 ontwikkeld door VNO-NCW Midden. Vanuit de leden kwam de vraag naar een makkelijke toegankelijke toolbox met instrumenten om direct in te zetten in hun eigen bedrijf en met voorbeelden van andere werkgevers. Deze versie is een 1.0 versie en de wens is om de toolbox breed uit te zetten onder ondernemers en door te ontwikkelen op basis van extra behoeftes van werkgevers.</p>
+                    <p> De HR toolbox is in 2023 ontwikkeld door VNO-NCW Midden. Vanuit de leden kwam de vraag naar een makkelijke toegankelijke toolbox met instrumenten om direct in te zetten in hun eigen bedrijf en met voorbeelden van andere werkgevers. Deze 1.0 versie is in september 2023 opgeleverd. De wens is om de toolbox breed uit te zetten onder ondernemers en door te ontwikkelen op basis van extra behoeftes van werkgevers.</p>
                     <p> De toolbox is ondersteund door RW-POA Rivierenland en die wil hiermee bereiken dat meer ondernemers een goed in-, door- en uitstroombeleid hebben voor hun medewerkers. Met als doel een optimaal werkende arbeidsmarkt en behoud van professionals.</p>
                     <p> Studenten van Roc Rivor ontwikkelden de website vanuit de digitale werkplaats: DigiLab Rivierenland. In het DigiLab gaan studenten samen met bedrijven uit de regio aan de slag met echte opdrachten. Studenten helpen bij de digitalisering van de bedrijfsvoering, leren in de praktijk en werken samen vanuit verschillende opleidingen.</p>
                 </div>
