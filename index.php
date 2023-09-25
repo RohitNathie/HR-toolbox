9ecce1a9949f78e928a81353cfea3ef8330b2efb
MarioPfaff <mariojuliaanpfaff@gmail.com>
2023-09-25 17:17:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,8 +20,8 @@
     <div class="homepage child-container">
       <h1 class="h-1 font-weight-bold" style="font-size: var(--banner-font-size);">HR TOOLBOX</h1>
       <p class="subtitle h-2 font-weight-800" style="color: var(--subtitle-font-color); font-size: var(--subtitle-font-size); font-weight: 600;">DE TOOLBOX VOOR HET MKB</p>
-      <p class="pt-2 pb-3">Welkom! Je hebt de HR-toolbox gevonden. je kunt hier instrumenten vinden die jouw helpen om nieuwe medewerkers te werven (instroom), te behouden (doorstroom) en goed los te laten (uitstroom).
-        Naast een eenvoudig instrument zijn er ook stappenplannen om het instrument in je organisatie in te bedden toegevoegd en kun je voorbeelden van werkgevers vinden die vertellen waarom het bij hun werkt.</p>
+      <p class="pt-2 pb-3">Welkom! Je hebt de HR-toolbox gevonden. Je vindt hier instrumenten die jou helpen om nieuwe medewerkers te werven (instroom), te behouden (doorstroom) en goed los te laten (uitstroom).
+      Ook zijn er stappenplannen toegevoegd die je helpen om het instrument in je organisatie in te bedden en vind je voorbeelden van werkgevers die vertellen waarom het bij hun werkt.</p>
       <p class="lead">
         <a class="btn text-uppercase" href="tool-selection.php" role="button">Zoek jouw tool</a>
       </p>
